b078f4929bff4d48651404139d8ba8934b254b5b
UygarKAYA <uygar.kaya@ozu.edu.tr>
2022-04-17 17:27:49 +0300

    Linear Regression models are finished

--- Algorithms/linearRegression.py
+++ Algorithms/linearRegression.py
@@ -1,11 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import r2_score
 
 # Read the data
 file_name = '../Dataset/WineQualityNew.csv'
@@ -21,10 +21,10 @@ df3 = dataFrame[['fixed acidity', 'free sulfur dioxide', 'total sulfur dioxide',
 df4 = dataFrame[['alcohol', 'pH', 'density', 'residual sugar', 'volatile acidity', 'sulphates', 'quality']]
 
 # split the data into training and testing data with using the independent variables(y) and dependent variable(x)
-X1_train, X1_test, Y1_train, Y1_test = train_test_split(df1.iloc[:,0:7].values, df1.iloc[:,-1].values, test_size=0.25, random_state=147)
-X2_train, X2_test, Y2_train, Y2_test = train_test_split(df2.iloc[:,0:7].values, df2.iloc[:,-1].values, test_size=0.25, random_state=147)
-X3_train, X3_test, Y3_train, Y3_test = train_test_split(df3.iloc[:,0:7].values, df3.iloc[:,-1].values, test_size=0.25, random_state=147)
-X4_train, X4_test, Y4_train, Y4_test = train_test_split(df4.iloc[:,0:7].values, df4.iloc[:,-1].values, test_size=0.25, random_state=147)
+X1_train, X1_test, Y1_train, Y1_test = train_test_split(df1.iloc[:,0:11].values, df1.iloc[:,-1].values, test_size=0.25, random_state=147)
+X2_train, X2_test, Y2_train, Y2_test = train_test_split(df2.iloc[:,0:6].values, df2.iloc[:,-1].values, test_size=0.25, random_state=147)
+X3_train, X3_test, Y3_train, Y3_test = train_test_split(df3.iloc[:,0:6].values, df3.iloc[:,-1].values, test_size=0.25, random_state=147)
+X4_train, X4_test, Y4_train, Y4_test = train_test_split(df4.iloc[:,0:6].values, df4.iloc[:,-1].values, test_size=0.25, random_state=147)
 
 x_train = [X1_train, X2_train, X3_train, X4_train]
 y_train = [Y1_train, Y2_train, Y3_train, Y4_train]
@@ -73,13 +73,34 @@ for index in range(len(linear_regression)):
 
 # Printing the each score of the model
 for index, value in enumerate(score):
-    print(f'Score{index+1}: {value}')
+    print(f'score{index+1}: {value}')
 
-# Calculate the mean squared error of the predicted values.
-# We want to get lower values
-mse = []
-for index in range(len(linear_regression)):
-    mse.append(mean_squared_error(y_test[index], prediction[index]))
+def calculate_error_rate(linear_regression, y_test, prediction):
+    # Calculate the mean squared error
+    mse = []
+    for index in range(len(linear_regression)):
+        mse.append(mean_squared_error(y_test[index], prediction[index]))
+    
+    # Calculate the mean absolute error
+    mae = []
+    for index in range(len(linear_regression)):
+        mae.append(mean_absolute_error(y_test[index], prediction[index]))
+    
+    return mse, mae
 
+# Printing the each error metrics of the model
+mse, mae = calculate_error_rate(linear_regression, y_test, prediction)
 for index, value in enumerate(mse):
-    print(f'mse{index+1}: {value}')
+    print(f'MSE{index+1}: {value}')
+for index, value in enumerate(mae):
+    print(f'MAE{index+1}: {value}')
+
+def visulize_error_rate(x_label, y_label, models, error_rate):
+    # Visulize the error rate
+    plt.xlabel(x_label, fontweight="bold", style="italic")
+    plt.ylabel(y_label, fontweight="bold", style="italic")
+    plt.scatter(models, error_rate, s=75, marker='o', color='b')
+    plt.show()
+
+visulize_error_rate('Models', 'Mean Squared Error', ["M{}".format(index+1) for index in range(4)], mse)
+visulize_error_rate('Models', 'Mean Absolute Error', ["M{}".format(index+1) for index in range(4)], mae)
